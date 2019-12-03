bae8f3e82eb457e2dd2f97399a05d1a0090761cc
Jacob McSwain <jacob@clevyr.com>
2019-12-03 11:30:29 -0600

    Add blacklist checking support to SES mailer service

--- main.py
+++ main.py
@@ -6,12 +6,33 @@ import boto3
 
 SES = boto3.client('ses')
 SQS = boto3.client('sqs')
+dynamodb = boto3.client('dynamodb')
 
 SES_RATE_LIMIT = int(os.getenv('AWS_SES_RATE_LIMIT', '10'))
 QUEUE_URL = os.getenv('AWS_SQS_URL', '')
+USE_BLACKLIST = os.getenv('USE_BLACKLIST', 'false').lower() == True
 
 # You can only pull 10 messages at a time max
 MAX_SQS_MESSAGES = SES_RATE_LIMIT if SES_RATE_LIMIT <= 10 else 10
+
+
+def removeBlacklist(email_addresses):
+    new_addresses = []
+    for email in email_addresses:
+        item = dynamodb.get_item(
+            Key={
+                'email': {
+                    'S': email,
+                },
+            },
+            TableName='ses_blacklist',
+        )
+        if 'Item' in item.keys():
+            print(f'Removing {email} from the sender list due to being blacklisted')
+        else:
+            new_addresses.append(email)
+    return new_addresses
+
 
 def main():
     print('Listening for emails')
@@ -26,6 +47,10 @@ def main():
             for message in response['Messages']:
                 email = json.loads(message['Body'])
                 if email is not None:
+                    if USE_BLACKLIST:
+                        # Filter out blacklisted email addresses
+                        email['Destination']['ToAddresses'] = removeBlacklist(email['Destination']['ToAddresses'])
+
                     SES.send_email(**email)
                     SQS.delete_message(
                         QueueUrl=QUEUE_URL,
@@ -37,6 +62,7 @@ def main():
                         dest = email['Destination']['ToAddresses']
                         print(f'Email sent to {dest}')
         time.sleep(2)
+
 
 if __name__ == "__main__":
     main()
